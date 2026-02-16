4b792ecca1c7fc254c10ba53bea7f387ad11dfe0
i-rajveer-singh <work.rajveerbagga@gmail.com>
2026-02-16 16:37:17 +0530

    [Rajveer]:UC4: Implemented defensive input validation before risk calculation

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -3,10 +3,28 @@ import java.util.*;
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter values");
         double armPrecision = sc.nextDouble();
         int workerDensity = sc.nextInt();
         String machineryState = sc.next();
+
+        if(armPrecision < 0.0 || armPrecision > 1.0){
+            System.out.println("Invalid arm precision");
+            return;
+        }
+
+        if(workerDensity < 1 || workerDensity > 20){
+            System.out.println("Invalid worker density");
+            return;
+        }
+
+        if(!(machineryState.equals("Worn") ||
+                machineryState.equals("Faulty") ||
+                machineryState.equals("Critical"))){
+
+            System.out.println("Invalid machinery state");
+            return;
+        }
 
         double machineRiskFactor = 1.3;
 
