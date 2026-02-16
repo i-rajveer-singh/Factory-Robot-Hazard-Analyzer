28467f08e6324f4efa1efdd5f5d279732e3bd2ae
i-rajveer-singh <work.rajveerbagga@gmail.com>
2026-02-16 16:14:03 +0530

    [Rajveer]:UC3:Added business logic for hazard risk scoring

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -8,8 +8,13 @@ public class FactoryRobotHazardAnalyzer {
         int workerDensity = sc.nextInt();
         String machineryState = sc.next();
 
-        System.out.println(armPrecision);
-        System.out.println(workerDensity);
-        System.out.println(machineryState);
+        double machineRiskFactor = 1.3;
+
+        if(machineryState.equals("Faulty")) machineRiskFactor = 2.0;
+        if(machineryState.equals("Critical")) machineRiskFactor = 3.0;
+
+        double hazardRisk = ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
+
+        System.out.println("Robot Hazard Risk Score: " + hazardRisk);
     }
 }
