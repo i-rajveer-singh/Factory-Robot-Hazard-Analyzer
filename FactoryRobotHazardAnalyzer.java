cd0a2861f8de48e035f6691b328028264be7d59c
i-rajveer-singh <work.rajveerbagga@gmail.com>
2026-02-16 17:31:02 +0530

    [Rajveer]:UC7:Added business rule encapsualtion for machinery state risk factors

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -9,33 +9,15 @@ public class FactoryRobotHazardAnalyzer {
         double armPrecision = sc.nextDouble();
         int workerDensity = sc.nextInt();
         String machineryState = sc.next();
+        RobotHazardAuditor auditor = new RobotHazardAuditor();
 
-        try{
-            double risk = calculateHazardRisk(armPrecision,workerDensity,machineryState);
+        try {
+            double risk = auditor.CalculateHazardRisk(armPrecision, workerDensity, machineryState);
             System.out.println("Robot Hazard Risk Score: " + risk);
-        } catch(RobotSafetyException e){
+        } catch (RobotSafetyException e) {
         }
 
-    }
-
-
-    // UC5
-    public static double calculateHazardRisk(double armPrecision,int workerDensity,String machineryState) throws RobotSafetyException
-    {
-        if(armPrecision < 0.0 || armPrecision > 1.0){
-            throw new RobotSafetyException("Error: Arm precision must be 0.0-1.0");
-        }
-
-        if(workerDensity < 1 || workerDensity > 20){
-            System.out.println("Invalid worker density");
-            return 0;
-        }
-
-        double machineRiskFactor = 1.3;
-
-        if(machineryState.equals("Faulty")) machineRiskFactor = 2.0;
-        if(machineryState.equals("Critical")) machineRiskFactor = 3.0;
-
-        return ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
     }
 }
+
+
